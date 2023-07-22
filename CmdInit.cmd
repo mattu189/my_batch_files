0d5e913c317ee6530bf004d884098d811a08086b
mattu189 <yoshitika183@gmail.com>
2023-07-23 08:40:28 +0900

    git bashに対応

--- CmdInit.cmd
+++ CmdInit.cmd
@@ -1,13 +1,8 @@
 @echo off
 PROMPT $E[96;40m$P$_$E[95;40m$G$E[97;40m$S$
 chcp 65001
-doskey ls=dir $*
-doskey ps=tasklist 
 doskey ..=cd ..
-doskey cat=type $*
-doskey cd=cddir $*
-doskey mv=move $*
-doskey rm=del $*
-doskey pwd=@cd
+doskey ls=ls -p --color=auto $*
 cd C:\Users\yoshi\Desktop
+doskey cd=cddir $*
 cls
